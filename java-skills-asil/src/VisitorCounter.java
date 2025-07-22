f56ced8d1e5a8e8e90d1316b498348d75d19c4c0
Asil2003 <asilalabri03@gmail.com>
2025-07-23 00:19:52 +0400

    Add VisitorCounter system with static and non-static behavior

--- java-skills-asil/src/VisitorCounter.java
+++ java-skills-asil/src/VisitorCounter.java
@@ -1,2 +1,39 @@
 public class VisitorCounter {
+    private static int totalVisitors = 0;
+    private int sessionVisits;
+    private String visitorName;
+
+    public VisitorCounter(String name) {
+        this.visitorName = name;
+        this.sessionVisits = 0;
+        totalVisitors++;
+    }
+
+    public void recordVisit() {
+        sessionVisits++;
+        System.out.println(visitorName + " visited. Session visits: " + sessionVisits);
+    }
+
+    public static void displayTotalVisitors() {
+        System.out.println("Total visitors today: " + totalVisitors);
+    }
+
+    public static int getTotalVisitors() {
+        return totalVisitors;
+    }
+
+    public static void main(String[] args) {
+        VisitorCounter v1 = new VisitorCounter("Asma");
+        VisitorCounter v2 = new VisitorCounter("Ahmed");
+        VisitorCounter v3 = new VisitorCounter("Mira");
+
+        v1.recordVisit();
+        v1.recordVisit();
+        v2.recordVisit();
+        v3.recordVisit();
+        v3.recordVisit();
+        v3.recordVisit();
+
+        VisitorCounter.displayTotalVisitors();
+    }
 }
